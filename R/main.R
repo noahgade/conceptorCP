d825e07cb9f0462f552cb415f0c5a0d027b50242
Noah Gade <ndg5e@virginia.edu>
2022-07-30 06:04:24 -0400

    Version 1.2 - Changed plot mechanism & updated warning structure

--- R/main.R
+++ R/main.R
@@ -38,12 +38,12 @@
 #' @importFrom dplyr %>%
 #'
 #' @examples
-#' \dontrun{
+#' \donttest{
 #' ccp(test_data, washL_plus_trainL = 150)
 #' ccp(test_data, trainL = 100)
 #' ccp(test_data, trainL = 100, tol = 0.08, nboots = 500)
 #' }
-ccp <- function(data, washL_plus_trainL = "", trainL = "", washoutL = "", tol = 0.04, nboots = 200) {
+ccp <- function(data, washL_plus_trainL = "", trainL = "", washoutL = "", tol = 0.04, nboots = 200, plot.it = TRUE) {
 
   if(check_integer(washL_plus_trainL) == FALSE & check_integer(trainL) == FALSE) {
     stop(cat("Error: Please specify a number of time points for conceptor training.\nSpecify one of the following parameters: washL_plus_trainL, trainL"))
@@ -78,145 +78,11 @@ ccp <- function(data, washL_plus_trainL = "", trainL = "", washoutL = "", tol = 
                  "statSeries" = KSseries,
                  "angles" = CRNNFit$output$angles,
                  "netParams" = append(CRNNFit$params, list(C = CRNNFit$output$C, W = CRNNFit$output$W, ResStates = CRNNFit$output$ResStates)))
+
+  if(plot.it == TRUE) {
+    CPplot <- plotCP(output)
+    print(CPplot)
+  }
+
   return(output)
-}
-
-#' Viaualize conceptor CP method
-#' @description Plots estimate and internal dynamics of the conceptor change point method.
-#'
-#' @details Plots the time-ordered series of Kolmogorov-Smiornov like statistics from the conceptor change point method along with quantiles of the moving block bootstrap null distribution. Provides a visual aid of the relationship between the computed conceptor spaces and the propagating reservoir states over time with a cosine similarity measure. A comparison of the empirical disribution functions for windows of the cosine similarities are also included. Shading is relative and not on the same scale for all plots. Red shading represents time points where the dynamics are further away from the original conceptor space, and blue shading represents dynamics closer to the training window and the conceptor space.
-#'
-#' @param ccp_output Output from the conceptorCP function.
-#' @param nbreaks Number of windows to divide series for visual.
-#'
-#' @return Plots in the following order:
-#' \itemize{
-#' \item \emph{Top}: Time-ordered series of statistics with most likely change point location and MBB results.
-#' \item \emph{Middle}: Time-ordered cosine similarities between reservoir states and conceptor space.
-#' \item \emph{Bottom}: Comparison of cosine similarity empirical CDFs (each window against the full time series).
-#' }
-#' @export
-#' @importFrom dplyr %>%
-#' @importFrom grDevices rgb
-#' @importFrom scales label_number
-#'
-#' @examples
-#' \dontrun{
-#' ccp_output <- ccp(test_data, washL_plus_trainL = 150)
-#' plotCP(ccp_output)
-#' }
-plotCP <- function(ccp_output, nbreaks = 10) {
-  Time <- Window <- Values <- Reference <- WindowLength <- RCDF <- NULL
-  Angles <- dplyr::tibble(Angles = ccp_output$angles)
-  L <- nrow(Angles)
-  Angles <- Angles %>% dplyr::mutate(Time = seq(1, L))
-  PWAngles <- Angles[(ccp_output$netParams$washoutL + ccp_output$netParams$trainL + 1):L,]
-  AngleMin <- min(PWAngles) - (1 - min(PWAngles)) / 100
-  PWAngles <- PWAngles %>% dplyr::mutate(PWRanks = rank(PWAngles$Angles) / nrow(PWAngles))
-  EndPts <- floor(seq(0, nrow(PWAngles), length.out = nbreaks + 1)) + ccp_output$netParams$washoutL + ccp_output$netParams$trainL
-  PWAngles <- PWAngles %>% dplyr::rowwise() %>% dplyr::mutate(Window = sum(EndPts < Time))
-  PWAngles$Values <- paste("Value", unlist(sapply(diff(EndPts), seq, simplify = F)))
-
-  plotM <- ggplot2::ggplot(PWAngles, ggplot2::aes_string(x = "Time", y = "Angles")) +
-    ggplot2::geom_segment(data = PWAngles, ggplot2::aes_string(x = "Time", xend = "Time", y = "AngleMin", yend = 1, color = "PWRanks")) +
-    ggplot2::scale_color_gradientn(name = "",
-                                   colors = c(rgb(1, 0, 0), rgb(1, 1, 1, 0), rgb(0, 0, 1)),
-                                   limits = c(0, 1), na.value = "white",
-                                   breaks = c(0, 0.5, 1),
-                                   labels = c("Away from \nConceptor \nSpace", "\nMiddle: Percentiles of Cosine Similarities \n\nBottom: Relative ECDF Difference", "Towards \nConceptor \nSpace")) +
-    ggplot2::geom_point(ggplot2::aes_string(x = "Time", y = "Angles"), shape = 20, size = 1) +
-    ggplot2::theme_bw() +
-    ggplot2::scale_x_continuous(name = "Time",
-                                expand = c(0, 0),
-                                breaks = EndPts + c(rep(1, nbreaks), 0)) +
-    ggplot2::scale_y_continuous(name = paste("Cosine Similaritiy\n Smaller  \u2194  Larger", sep = ''),
-                                labels = label_number(accuracy = 0.1),
-                                limits = c(AngleMin, 1),
-                                breaks = 1,
-                                expand = c(0, 0)) +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(color = "white", size = 2),
-                   axis.ticks.y = ggplot2::element_blank(),
-                   legend.position = "bottom",
-                   legend.text = ggplot2::element_text(size = 10),
-                   axis.title.x = ggplot2::element_text(size = 10),
-                   axis.text.x = ggplot2::element_text(size = 10),
-                   axis.title.y = ggplot2::element_text(size = 10),
-                   legend.justification = "top",
-                   plot.margin = ggplot2::unit(c(0.2, 0.4, 0.2, 0.2), "cm"),
-                   legend.key.height = ggplot2::unit(0.3, "cm"),
-                   legend.key.width = ggplot2::unit(2, "cm"),
-                   panel.grid.major = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank())
-
-  CDF <- dplyr::tibble(Reference = rep(sort(PWAngles$Angles), nbreaks), RCDF = rep(seq(nrow(PWAngles)) / nrow(PWAngles), nbreaks), Window = rep(seq(1, nbreaks), each = nrow(PWAngles)), WindowLength = rep(diff(EndPts), each = nrow(PWAngles)))
-  WCDF <- dplyr::select(PWAngles, Angles, Window, Values) %>% tidyr::pivot_wider(names_from = Values, values_from = Angles)
-  WCDF <- dplyr::left_join(CDF, WCDF, by = "Window")
-  WCDF <- dplyr::transmute(WCDF, dplyr::across(5:max(diff(EndPts)), function(X) X <= Reference))
-  CDF$WCDF <- dplyr::rowwise(WCDF) %>% dplyr::transmute(WCDF = sum(dplyr::c_across(cols = dplyr::everything())))
-  CDF <- dplyr::mutate(CDF, WCDF = WCDF$WCDF / WindowLength, Shading = RCDF - WCDF)
-
-  plotB <- ggplot2::ggplot(data = CDF, ggplot2::aes_string(x = "RCDF", y = "WCDF")) +
-    ggplot2::geom_segment(ggplot2::aes_string(x = "RCDF", xend = "RCDF", y = 0, yend = 1, color = "Shading")) +
-    ggplot2::scale_color_gradientn(name = "",
-                                   colors = c(rgb(1, 0, 0), rgb(1, 1, 1, 0.5), rgb(0, 0, 1)),
-                                   limits = c(-1, 1), na.value = "white") +
-    ggplot2::facet_wrap(. ~ Window, nrow = 1) +
-    ggplot2::geom_point(data = CDF, ggplot2::aes_string(x = "RCDF", y = "WCDF"), col = "black", shape = 20, size = 0.2) +
-    ggplot2::scale_x_continuous(name = "ECDF (Full Time Series)", limits = c(0, 1), expand = c(0, 0)) +
-    ggplot2::scale_y_continuous(name = "ECDF (Time Window)", limits = c(0, 1), expand = c(0, 0)) +
-    ggplot2::theme(strip.text.x = ggplot2::element_blank(),
-                   strip.text.y = ggplot2::element_blank(),
-                   strip.background = ggplot2::element_rect(color = "black", fill = "white"),
-                   panel.spacing.y = ggplot2::unit(0.05, "cm"),
-                   panel.spacing.x = ggplot2::unit(0, "cm"),
-                   panel.grid.major = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank(),
-                   legend.key.width = ggplot2::unit(0.3, "cm"),
-                   legend.key.height = ggplot2::unit(0.5, "cm"),
-                   axis.text.y = ggplot2::element_blank(),
-                   axis.title.y = ggplot2::element_text(size = 10),
-                   legend.justification = "bottom",
-                   axis.text.x = ggplot2::element_blank(),
-                   axis.title.x = ggplot2::element_text(size = 10),
-                   legend.position = "none",
-                   plot.margin = ggplot2::unit(c(0.2, 0.4, 0.2, 0.2), "cm"),
-                   axis.ticks.y = ggplot2::element_blank(),
-                   axis.ticks.x = ggplot2::element_blank())
-
-  SSeries <- dplyr::tibble(Time = seq(ccp_output$netParams$washoutL + ccp_output$netParams$trainL + 1, L), Stat = ccp_output$statSeries)
-  upper.limit <- max(SSeries$Stat, stats::quantile(ccp_output$MBBnull, 0.99)[[1]]) + 0.25
-
-  plotT <- ggplot2::ggplot(SSeries) +
-    ggplot2::geom_line(ggplot2::aes_string(x = "Time", y = "Stat")) +
-    ggplot2::geom_hline(ggplot2::aes(yintercept = stats::quantile(ccp_output$MBBnull, 0.90)[[1]], linetype = "Upper 10% \nMBB Null Dist."), color = "red", key_glyph = "cust") +
-    ggplot2::geom_hline(ggplot2::aes(yintercept = stats::quantile(ccp_output$MBBnull, 0.95)[[1]], linetype = "Upper 5% \nMBB Null Dist."), color = "red", key_glyph = "cust") +
-    ggplot2::geom_hline(ggplot2::aes(yintercept = stats::quantile(ccp_output$MBBnull, 0.99)[[1]], linetype = "Upper 1% \nMBB Null Dist."), color = "red", key_glyph = "cust") +
-    ggplot2::geom_vline(ggplot2::aes(xintercept = ccp_output$estimate, linetype = "Most Likely \nChange Point"), color = "blue", key_glyph = "cust") +
-    ggplot2::scale_linetype_manual("", values = c("Most Likely \nChange Point" = "solid", "Upper 10% \nMBB Null Dist." = "dotted", "Upper 5% \nMBB Null Dist." = "dashed", "Upper 1% \nMBB Null Dist." = "longdash"),
-                                   guide = ggplot2::guide_legend(override.aes = list(colour = c("blue", "red", "red", "red")))) +
-    ggplot2::scale_x_continuous(name = "Time", limits = c(ccp_output$netParams$washoutL + ccp_output$netParams$trainL + 1, L),
-                                expand = c(0, 0), EndPts + c(rep(1, nbreaks), 0)) +
-    ggplot2::scale_y_continuous(name = "Statistic", limits = c(0, upper.limit), labels = label_number(accuracy = 0.1),
-                                expand = c(0, 0)) + ggplot2::theme_bw() +
-    ggplot2::theme(axis.text.y = ggplot2::element_text(size = 10),
-                   legend.position = "top",
-                   legend.justification = "right",
-                   axis.title.x = ggplot2::element_text(size = 10),
-                   axis.text.x = ggplot2::element_text(size = 10),
-                   axis.title.y = ggplot2::element_text(size = 10),
-                   plot.margin = ggplot2::unit(c(0.2, 0.4, 0.2, 0.2), "cm"),
-                   plot.caption = ggplot2::element_text(size = 12),
-                   panel.grid.major = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank())
-
-  plotS <- cowplot::axis_canvas(plotT, axis = "y", coord_flip = TRUE) +
-    ggplot2::geom_histogram(ggplot2::aes(x = ccp_output$MBBnull), binwidth = 0.5, color = "black", fill = rgb(1, 0, 0, 0.2)) +
-    ggplot2::coord_flip()
-  suppressWarnings({
-  plot1 <- cowplot::insert_yaxis_grob(plotT, plotS, grid::unit(0.1, "null"), position = "right")
-  plot2 <- cowplot::plot_grid(plotM + ggplot2::theme(legend.position = "none"), plotB, ncol = 1, align = "hv", axis = "lr", rel_heights = c(1.1, 1))
-  plot3 <- cowplot::insert_yaxis_grob(plot2, grid::nullGrob(), grid::unit(0.1, "null"), position = "right")
-  plot4 <- cowplot::plot_grid(plot1, plot3, ncol = 1)
-  plot5 <- cowplot::plot_grid(plot4, cowplot::get_legend(plotM), rel_heights = c(1, 0.15), ncol = 1, align = "hv", axis = "r")})
-  return(plot5)
 }
